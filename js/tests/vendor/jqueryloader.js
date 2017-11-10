e6c691442a3546eabcc2ca84b944e850dd4f670e
Johann-S <johann.servoire@gmail.com>
2017-11-10 11:25:47 +0100

    Allow us to run our unit test on differents jquery version

---
+++ js/tests/vendor/jqueryloader.js
@@ -0,0 +1,11 @@
+(function () {
+  var path = '../../assets/js/vendor/jquery-slim.min.js'
+  // get jquery param from the query string.
+  var jQueryVersion = location.search.match(/[?&]jquery=(.*?)(?=&|$)/)
+
+  // If a version was specified, use that version from or vendor folder
+  if (jQueryVersion) {
+    path = 'vendor/jquery-' + jQueryVersion[1] + '.min.js'
+  }
+  document.write('<script src="' + path + '"></script>')
+}());
